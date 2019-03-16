94f2ed3f999f3bf4b5e72645f92413c5d96d91c2
javiercordero <javier.cordero@upr.edu>
2019-03-16 12:46:12 -0400

    first commit

---
+++ backend/api/controllers/PlantsController.js
@@ -0,0 +1,16 @@
+/**
+ * PlantsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+    optimize: async function(req, res) {
+        //
+    }
+
+
+};
+
